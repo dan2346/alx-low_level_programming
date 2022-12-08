68752e517b534dd0e2b6b6f6e45045458edc5102
dan2346 <117739564+dan2346@users.noreply.github.com>
2022-12-09 01:10:06 +0300

    Update 5-printf.c
    print 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,17 +1,10 @@
 #include <studio.h>
-
 /**
  * main -Entry
- * return: always 0
+ * Return: Always 0
 */
-
-
 int main(void)
-
-
-{	
-
+{
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
-
 }
