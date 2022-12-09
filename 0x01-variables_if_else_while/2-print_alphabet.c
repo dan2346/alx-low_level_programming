fe08f0b861416fe41ee6fe5ae0321e7a8513d435
dan2346 <danielndungu2346@gmail.com>
2022-12-09 11:23:31 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints alpha in low case
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char lc;
+
+	for (lc = 'a'; lc <= 'z'; lc++);
+	{
+		putchar(lc);
+	}
+	putchar('\n');
+	return (0);
+}
