8e9770448237cf25364089e3b3d7facf659b3710
dan2346 <117739564+dan2346@users.noreply.github.com>
2022-12-13 22:15:05 +0300

    Update 100-print_comb3.c
    comb 4

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,24 +7,23 @@
 
 int main(void)
 {
-	int n;
-	int m;
+	int n = '0';
+	int m = '0';
 
-	for (n = 0; n <= 9; n++)
+	for (m = '0'; m <= '9'; m++)
 	{
-		for (m = 0; m <= 9; m++)
+		for (n = '0'; n <= '9'; n++)
 		{
 			if (!((n == m) || (m > n)))
 			{
-				putchar(n);
 				putchar(m);
-				if (!(n == 9 && m == 8))
+				putchar(n);
+				if (!(n == '9' && m == '8'))
 				{
 					putchar(',');
 					putchar(' ');
 				}
-			}	
-			
+			}
 		}
 	}
 	putchar('\n);
