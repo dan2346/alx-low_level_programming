c6fe7360ba67295f82db95760a2a52e4f890d3c0
dan2346 <117739564+dan2346@users.noreply.github.com>
2022-12-13 22:54:26 +0300

    Update 101-print_comb4.c
    combination 1

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,5 +7,29 @@
 
 int main(void)
 {
-
+  int n = '0';
+  int m = '0';
+  int k = '0';
+  for (k = '0'; k <='9'; k++)
+  {
+    for (m ='0'; m <= '9'; m++)
+    {
+      for (n = '0'; n <='9'; n++)
+      {
+        if (!((n == m) || (m == k) || (m > n) || (k > m)))
+        {
+          putchar(k);
+          putchar(m);
+          putchar(n);
+          if (!(n == '9' && k == '7' && , == '8')
+          {
+            putchar(',');
+            putchar(' ');
+           }
+          }
+         }
+        }
+       }
+      putchar('\n');
+      return (0);
 }
