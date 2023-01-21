ddb8429c021e096bc65502ccecf673490f4d5567
dan2346 <danielndungu2346@gmail.com>
2023-01-21 03:58:37 -0800

    variadic

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_H
-#include MAIN_H
+#ifndef _VARIADIC_H_
+#define _VARIADIC_H_
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
