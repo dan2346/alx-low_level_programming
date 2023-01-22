86d5e52f6de8f204cd6ebc919a28de50c304571e
dan2346 <117739564+dan2346@users.noreply.github.com>
2023-01-22 15:10:17 +0300

    Update 1-string_nconcat.c
    try 4

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,7 +10,7 @@
  * Return: pointer to the resulting string
  */
 
-char *string_nconcat(char *s1, char *s2, unsigned int n)	
+char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *s;
 
@@ -19,7 +19,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	while (s1 && s1[len1])
 		len1++;
 
-    while (s2 && s2[len2])
+	while (s2 && s2[len2])
 		len2++;
 
 	if (n < len2)
